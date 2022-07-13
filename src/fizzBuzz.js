98380f70b28d2044afab28a8adb8889678453795
AaronRodrigues <aaronrod@gmail.com>
2022-07-13 11:26:11 +0100

    Setup

---
+++ src/fizzBuzz.js
@@ -0,0 +1,11 @@
+// Write a function that takes a number as an argument and then returns the number
+// However:
+// For every multiple of 3, print “Fizz” instead of the number.
+// For every multiple of 5, print “Buzz” instead of the number.
+// For every multiple of both 3 and 5, print “FizzBuzz” instead of the number.
+
+const fizzBuzz = () => {
+  
+};
+
+module.exports = fizzBuzz;
